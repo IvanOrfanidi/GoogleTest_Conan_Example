5be2ea37ca1e42d81f60abcc2d9350f06405a332
Ivan Orfanidi <i.orfanidi@mail.ru>
2020-11-15 15:00:52 +0300

    Подправил файл сценария сборки conan тестов.

--- compile_googletest.sh
+++ compile_googletest.sh
@@ -3,10 +3,4 @@ mkdir -p build
 cd build
 
 # Build Test
-if [[ "$OSTYPE" == "msys" ]]; then
-    # For Windows
-    conan install ../ -s build_type=Debug --build=gtest
-else
-    # For Linux or other OS
-    conan install ../ -s build_type=Debug
-fi
+conan install ../ -s build_type=Debug
