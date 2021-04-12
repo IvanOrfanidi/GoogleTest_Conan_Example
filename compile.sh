acb89db8f9cc7a00909ca1fd8467cd4d86ccd11b
Ivan Orfanidi <i.orfanidi@mail.ru>
2021-04-13 02:43:32 +0300

    Подправлены скрипты сборки. Добавлена сборка с компилятором clang.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-mkdir -p build
+
+rm -rf build
+mkdir build
 cd build
 
+# Build Test
+conan install ../ -s build_type=Debug
+
 # Build Project
-cmake -G "Unix Makefiles" ..
-cmake --build . -- -j4
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DTEST=true ..
+cmake --build .
--- compile_googletest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p build
-cd build
-
-# Build Test
-conan install ../ -s build_type=Debug
